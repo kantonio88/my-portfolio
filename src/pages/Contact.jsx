dec1b3652715aeba8900ab118e147522427f8d4e
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-07-04 17:16:01 +0200

    Updated some more

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="container mx-auto px-4 py-8 pt-4">
-      <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
-      <p className="text-lg">Feel free to get in touch with me. You can reach me via email or through the social media links below:</p>
-      <ul className="mt-4 space-y-2">
-        <li>Email: keston.antonio.schmidtgen@gmail.com</li>
-        {/* Add other contact details */}
-      </ul>
+    <div>
+      <div className="text-center mt-4">
+        <h1 className="text-4xl font-bold mb-4">Contact Me</h1>
+        <p className="text-lg">Feel free to get in touch with me. You can reach me via email or through the social media links below:</p>
+      </div>
+    <div className="container d-flex justify-content-center align-items-center h-100 pt-4">
+      <div className="card w-50">
+        <div className="card-body">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">Message</label>
+              <textarea className="form-control" id="message" rows="5"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,9 +4,30 @@ const Experience = () => {
   return (
     <div className="container mx-auto px-4 py-8 pt-4">
       <h1 className="text-4xl font-bold mb-4">Experience</h1>
-      {/* Add your work experience details here */}
+      <div className="card mb-4">
+        <div className="card-body">
+          <h5 className="card-title">Software Engineer</h5>
+          <h6 className="card-subtitle mb-2 text-muted">ABC Company</h6>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit, urna vel rutrum hendrerit, dui leo dapibus lectus, vel fringilla est leo sit amet metus.</p>
+        </div>
+      </div>
+      <div className="card mb-4">
+        <div className="card-body">
+          <h5 className="card-title">Frontend Developer</h5>
+          <h6 className="card-subtitle mb-2 text-muted">XYZ Corporation</h6>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit, urna vel rutrum hendrerit, dui leo dapibus lectus, vel fringilla est leo sit amet metus.</p>
+        </div>
+      </div>
+      <div className="card mb-4">
+        <div className="card-body">
+          <h5 className="card-title">UI/UX Designer</h5>
+          <h6 className="card-subtitle mb-2 text-muted">123 Design Studio</h6>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit, urna vel rutrum hendrerit, dui leo dapibus lectus, vel fringilla est leo sit amet metus.</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Experience;
+
